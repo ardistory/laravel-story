b47a1b5cfafbb8e2dbfefe92c7eeeb6c8115926a
Ardiansyah Putra <putraardiansyah530@gmail.com>
2024-01-18 22:06:28 +0700

    System List Mac-Address

--- app/Livewire/ListMac.php
+++ app/Livewire/ListMac.php
@@ -2,10 +2,46 @@
 
 namespace App\Livewire;
 
+use App\Api\RouterosAPI;
+use App\Models\TokoLbk;
+use Livewire\Attributes\On;
 use Livewire\Component;
 
 class ListMac extends Component
 {
+    public array $listMac = [];
+    public string $ipWdcpProperty = '';
+    public string $storeCodeFromEvent = '';
+    public string $countMac = '';
+
+    #[On('submitTableToko')]
+    public function getListMacAddressFromRouter($kode_toko): void
+    {
+        $this->storeCodeFromEvent = $kode_toko;
+
+        $query = TokoLbk::query()->where('kode_toko', '=', $this->storeCodeFromEvent)->first();
+
+        try {
+            $this->ipWdcpProperty = $query->ip_wdcp;
+
+            $api = new RouterosAPI();
+
+            if ($api->connect($this->ipWdcpProperty, env('ROS_WDCP_USERNAME'), env('ROS_WDCP_PASSWORD'))) {
+                $data = $api->comm('/interface/wireless/access-list/print');
+
+                $this->countMac = count($data);
+                $this->listMac = $data;
+
+                session()->flash('counted', $this->countMac);
+            } else {
+                session()->flash('listMacFailed');
+            }
+        } catch (\Exception $exception) {
+            session()->flash('listMacFailed');
+        }
+
+    }
+
     public function render()
     {
         return view('livewire.list-mac');
--- resources/views/livewire/list-mac.blade.php
+++ resources/views/livewire/list-mac.blade.php
@@ -1,111 +1,52 @@
 <div>
     <div class="flex items-center justify-between border-b-[1px]">
-        <div class="font-semibold text-md px-4 py-4 flex items-center gap-2">
-            <div>
-                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
-                    stroke="currentColor" class="w-6 h-6">
-                    <path stroke-linecap="round" stroke-linejoin="round"
-                        d="M9 12h3.75M9 15h3.75M9 18h3.75m3 .75H18a2.25 2.25 0 0 0 2.25-2.25V6.108c0-1.135-.845-2.098-1.976-2.192a48.424 48.424 0 0 0-1.123-.08m-5.801 0c-.065.21-.1.433-.1.664 0 .414.336.75.75.75h4.5a.75.75 0 0 0 .75-.75 2.25 2.25 0 0 0-.1-.664m-5.8 0A2.251 2.251 0 0 1 13.5 2.25H15c1.012 0 1.867.668 2.15 1.586m-5.8 0c-.376.023-.75.05-1.124.08C9.095 4.01 8.25 4.973 8.25 6.108V8.25m0 0H4.875c-.621 0-1.125.504-1.125 1.125v11.25c0 .621.504 1.125 1.125 1.125h9.75c.621 0 1.125-.504 1.125-1.125V9.375c0-.621-.504-1.125-1.125-1.125H8.25ZM6.75 12h.008v.008H6.75V12Zm0 3h.008v.008H6.75V15Zm0 3h.008v.008H6.75V18Z" />
-                </svg>
+        <div class="w-full font-semibold text-md px-4 py-4 flex items-center justify-between gap-2">
+            <div class="flex gap-2 items-center">
+                <div>
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+                        stroke="currentColor" class="w-6 h-6">
+                        <path stroke-linecap="round" stroke-linejoin="round"
+                            d="M9 12h3.75M9 15h3.75M9 18h3.75m3 .75H18a2.25 2.25 0 0 0 2.25-2.25V6.108c0-1.135-.845-2.098-1.976-2.192a48.424 48.424 0 0 0-1.123-.08m-5.801 0c-.065.21-.1.433-.1.664 0 .414.336.75.75.75h4.5a.75.75 0 0 0 .75-.75 2.25 2.25 0 0 0-.1-.664m-5.8 0A2.251 2.251 0 0 1 13.5 2.25H15c1.012 0 1.867.668 2.15 1.586m-5.8 0c-.376.023-.75.05-1.124.08C9.095 4.01 8.25 4.973 8.25 6.108V8.25m0 0H4.875c-.621 0-1.125.504-1.125 1.125v11.25c0 .621.504 1.125 1.125 1.125h9.75c.621 0 1.125-.504 1.125-1.125V9.375c0-.621-.504-1.125-1.125-1.125H8.25ZM6.75 12h.008v.008H6.75V12Zm0 3h.008v.008H6.75V15Zm0 3h.008v.008H6.75V18Z" />
+                    </svg>
+                </div>
+                List Mac
             </div>
-            List Mac
+            @if (session('counted'))
+                <div>Count : {{ session('counted') }}</div>
+            @endif
+            @if (session('listMacFailed'))
+                <div class="flex items-center gap-2">
+                    <div class="flex-none rounded-full bg-red-500/20 p-1">
+                        <div class="h-1.5 w-1.5 rounded-full bg-red-500 animate-ping absolute">
+                        </div>
+                        <div class="h-1.5 w-1.5 rounded-full bg-red-500"></div>
+                    </div>
+                    <p class="text-xs leading-5 text-white">Offline</p>
+                </div>
+            @endif
         </div>
     </div>
     <div class="h-[130px] pb-2 w-full overflow-auto scrollbar-thin scrollbar-thumb-white">
-        <div class="flex gap-2 mx-2">
-            <div
-                class="w-[70%] h-5 mx-auto mt-[10px] bg-white rounded-xl text-black flex items-center justify-center gap-2">
-                00:00:00:00:00:00
-            </div>
-            <div
-                class="text-xs font-pixel w-[30%] h-5 mx-auto mt-[10px] bg-white rounded-xl text-black flex items-center justify-center gap-2">
-                IC
-            </div>
-            <button
-                class="hover:ring hover:ring-red-300 w-[30%] h-5 mx-auto mt-[10px] bg-red-500 rounded-xl text-white flex items-center justify-center gap-2">
-                <div>
-                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
-                        stroke="currentColor" class="w-6 h-6">
-                        <path stroke-linecap="round" stroke-linejoin="round" d="M6 18 18 6M6 6l12 12" />
-                    </svg>
+        @foreach ($listMac as $data)
+            <div class="flex gap-2 mx-2">
+                <div
+                    class="w-[70%] h-5 mx-auto mt-[10px] bg-white rounded-xl text-black font-semibold flex items-center justify-center gap-2">
+                    {{ $data['mac-address'] }}
                 </div>
-            </button>
-        </div>
-        <div class="flex gap-2 mx-2">
-            <div
-                class="w-[70%] h-5 mx-auto mt-[10px] bg-white rounded-xl text-black flex items-center justify-center gap-2">
-                00:00:00:00:00:00
-            </div>
-            <div
-                class="text-xs font-pixel w-[30%] h-5 mx-auto mt-[10px] bg-white rounded-xl text-black flex items-center justify-center gap-2">
-                IC
-            </div>
-            <button
-                class="hover:ring hover:ring-red-300 w-[30%] h-5 mx-auto mt-[10px] bg-red-500 rounded-xl text-white flex items-center justify-center gap-2">
-                <div>
-                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
-                        stroke="currentColor" class="w-6 h-6">
-                        <path stroke-linecap="round" stroke-linejoin="round" d="M6 18 18 6M6 6l12 12" />
-                    </svg>
+                <div
+                    class="text-xs font-pixel w-[30%] h-5 mx-auto mt-[10px] bg-white rounded-xl text-black flex items-center justify-center gap-2">
+                    {{ $data['comment'] ?? '' }}
                 </div>
-            </button>
-        </div>
-        <div class="flex gap-2 mx-2">
-            <div
-                class="w-[70%] h-5 mx-auto mt-[10px] bg-white rounded-xl text-black flex items-center justify-center gap-2">
-                00:00:00:00:00:00
+                <button
+                    class="hover:ring hover:ring-red-300 w-[30%] h-5 mx-auto mt-[10px] bg-red-500 rounded-xl text-white flex items-center justify-center gap-2">
+                    <div>
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+                            stroke="currentColor" class="w-6 h-6">
+                            <path stroke-linecap="round" stroke-linejoin="round" d="M6 18 18 6M6 6l12 12" />
+                        </svg>
+                    </div>
+                </button>
             </div>
-            <div
-                class="text-xs font-pixel w-[30%] h-5 mx-auto mt-[10px] bg-white rounded-xl text-black flex items-center justify-center gap-2">
-                IC
-            </div>
-            <button
-                class="hover:ring hover:ring-red-300 w-[30%] h-5 mx-auto mt-[10px] bg-red-500 rounded-xl text-white flex items-center justify-center gap-2">
-                <div>
-                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
-                        stroke="currentColor" class="w-6 h-6">
-                        <path stroke-linecap="round" stroke-linejoin="round" d="M6 18 18 6M6 6l12 12" />
-                    </svg>
-                </div>
-            </button>
-        </div>
-        <div class="flex gap-2 mx-2">
-            <div
-                class="w-[70%] h-5 mx-auto mt-[10px] bg-white rounded-xl text-black flex items-center justify-center gap-2">
-                00:00:00:00:00:00
-            </div>
-            <div
-                class="text-xs font-pixel w-[30%] h-5 mx-auto mt-[10px] bg-white rounded-xl text-black flex items-center justify-center gap-2">
-                IC
-            </div>
-            <button
-                class="hover:ring hover:ring-red-300 w-[30%] h-5 mx-auto mt-[10px] bg-red-500 rounded-xl text-white flex items-center justify-center gap-2">
-                <div>
-                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
-                        stroke="currentColor" class="w-6 h-6">
-                        <path stroke-linecap="round" stroke-linejoin="round" d="M6 18 18 6M6 6l12 12" />
-                    </svg>
-                </div>
-            </button>
-        </div>
-        <div class="flex gap-2 mx-2">
-            <div
-                class="w-[70%] h-5 mx-auto mt-[10px] bg-white rounded-xl text-black flex items-center justify-center gap-2">
-                00:00:00:00:00:00
-            </div>
-            <div
-                class="text-xs font-pixel w-[30%] h-5 mx-auto mt-[10px] bg-white rounded-xl text-black flex items-center justify-center gap-2">
-                IC
-            </div>
-            <button
-                class="hover:ring hover:ring-red-300 w-[30%] h-5 mx-auto mt-[10px] bg-red-500 rounded-xl text-white flex items-center justify-center gap-2">
-                <div>
-                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
-                        stroke="currentColor" class="w-6 h-6">
-                        <path stroke-linecap="round" stroke-linejoin="round" d="M6 18 18 6M6 6l12 12" />
-                    </svg>
-                </div>
-            </button>
-        </div>
+        @endforeach
     </div>
 </div>
