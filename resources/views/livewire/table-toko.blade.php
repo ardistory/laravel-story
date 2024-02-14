4b639bbdca90a7016a2c1e0ddeda08738acbfdbe
Edp Network G157 <edpnetlbk@outlook.com>
2024-02-15 06:38:04 +0700

    Fix: Set h-5 Picture table-toko.blade.php

--- resources/views/livewire/table-toko.blade.php
+++ resources/views/livewire/table-toko.blade.php
@@ -68,10 +68,10 @@
                             class="flex items-center gap-2 font-medium text-sm md:text-lg tracking-tight text-gray-900 leading-tight">
                             <div>
                                 @empty($data->picture)
-                                    <img class="w-5 h-auto rounded-full bg-cover"
+                                    <img class="w-5 h-5 rounded-full bg-cover"
                                         src="{{ asset('storage/img/profile/default.png') }}">
                                 @else
-                                    <img class="w-5 h-auto rounded-full"
+                                    <img class="w-5 h-5 rounded-full"
                                         src="{{ asset('storage/img/profile/' . $data->picture) }}">
                                 @endempty
                             </div>
