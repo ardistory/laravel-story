112232bdf95cabc6718c1ff71e0a6a5570669ef6
Ardiansyah Putra <putraardiansyah530@gmail.com>
2024-02-15 10:13:00 +0700

    Update: Leaderboard On Checklist

--- resources/views/livewire/checklist.blade.php
+++ resources/views/livewire/checklist.blade.php
@@ -1,22 +1,40 @@
 <div>
-    <div class="mb-4 flex justify-between ring-1 px-2 py-1 hover:ring">
+    <div
+        class="group mb-4 flex justify-between ring-1 ring-white px-2 py-1 hover:bg-white hover:text-black rounded-sm transition duration-200 cursor-pointer">
         <div class="flex items-center gap-2">
-            <div>
+            <div class="text-yellow-500">
                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
                     stroke="currentColor" class="w-6 h-6">
                     <path stroke-linecap="round" stroke-linejoin="round"
-                        d="M7.5 14.25v2.25m3-4.5v4.5m3-6.75v6.75m3-9v9M6 20.25h12A2.25 2.25 0 0 0 20.25 18V6A2.25 2.25 0 0 0 18 3.75H6A2.25 2.25 0 0 0 3.75 6v12A2.25 2.25 0 0 0 6 20.25Z" />
+                        d="M3 13.125C3 12.504 3.504 12 4.125 12h2.25c.621 0 1.125.504 1.125 1.125v6.75C7.5 20.496 6.996 21 6.375 21h-2.25A1.125 1.125 0 0 1 3 19.875v-6.75ZM9.75 8.625c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125v11.25c0 .621-.504 1.125-1.125 1.125h-2.25a1.125 1.125 0 0 1-1.125-1.125V8.625ZM16.5 4.125c0-.621.504-1.125 1.125-1.125h2.25C20.496 3 21 3.504 21 4.125v15.75c0 .621-.504 1.125-1.125 1.125h-2.25a1.125 1.125 0 0 1-1.125-1.125V4.125Z" />
                 </svg>
             </div>
             Leaderboard
+            <div
+                class="animate-pulse group-hover:text-red-500 group-hover:translate-x-5 group-hover:transition group-hover:duration-700">
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+                    stroke="currentColor" class="w-6 h-6">
+                    <path stroke-linecap="round" stroke-linejoin="round"
+                        d="m5.25 4.5 7.5 7.5-7.5 7.5m6-15 7.5 7.5-7.5 7.5" />
+                </svg>
+            </div>
         </div>
         <div class="flex items-center gap-2">
             <div>
                 <img class="w-8 h-8 rounded-full" src="{{ asset('storage/img/profile/' . Auth::user()->picture) }}">
             </div>
             <div class="flex flex-col">
-                <span class="text-zinc-500 text-xs">2015171331</span>
                 <span class="font-semibold">Ardiansyah Putra</span>
+                <span class="text-zinc-500 text-xs inline-flex gap-1 items-center">
+                    10000
+                    <div>
+                        <svg class="w-4 h-4" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+                            stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                            <path stroke-linecap="round" stroke-linejoin="round"
+                                d="M12 6v12m-3-2.818.879.659c1.171.879 3.07.879 4.242 0 1.172-.879 1.172-2.303 0-3.182C13.536 12.219 12.768 12 12 12c-.725 0-1.45-.22-2.003-.659-1.106-.879-1.106-2.303 0-3.182s2.9-.879 4.006 0l.415.33M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
+                        </svg>
+                    </div>
+                </span>
             </div>
         </div>
     </div>
