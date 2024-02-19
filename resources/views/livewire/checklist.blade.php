baee84b01399b370f303d36bdfe42169c1c977c1
Ardiansyah Putra <putraardiansyah530@gmail.com>
2024-02-19 16:12:14 +0700

    Fix: List Reject check_by_name

--- resources/views/livewire/checklist.blade.php
+++ resources/views/livewire/checklist.blade.php
@@ -618,13 +618,14 @@
                                             {{ $listRe['nama_toko'] }}
                                         </div>
                                         <div
-                                            class="text-red-500 bg-red-950 text-xs px-2 ring-1 ring-red-500 flex gap-2 items-center rounded-md">
+                                            class="text-red-500 bg-red-950 text-xs px-2 flex gap-1 items-center rounded-md">
                                             {{ $listRe['check_by_name'] }}
-                                            <div>
-                                                <svg class="w-4 h-4" xmlns="http://www.w3.org/2000/svg"
+                                            <div class="text-red-500">
+                                                <svg class="w-4 h-4 animate-pulse" xmlns="http://www.w3.org/2000/svg"
                                                     viewBox="0 0 24 24" fill="currentColor" class="w-6 h-6">
+                                                    <path d="M12 15a3 3 0 1 0 0-6 3 3 0 0 0 0 6Z" />
                                                     <path fill-rule="evenodd"
-                                                        d="M5.47 5.47a.75.75 0 0 1 1.06 0L12 10.94l5.47-5.47a.75.75 0 1 1 1.06 1.06L13.06 12l5.47 5.47a.75.75 0 1 1-1.06 1.06L12 13.06l-5.47 5.47a.75.75 0 0 1-1.06-1.06L10.94 12 5.47 6.53a.75.75 0 0 1 0-1.06Z"
+                                                        d="M1.323 11.447C2.811 6.976 7.028 3.75 12.001 3.75c4.97 0 9.185 3.223 10.675 7.69.12.362.12.752 0 1.113-1.487 4.471-5.705 7.697-10.677 7.697-4.97 0-9.186-3.223-10.675-7.69a1.762 1.762 0 0 1 0-1.113ZM17.25 12a5.25 5.25 0 1 1-10.5 0 5.25 5.25 0 0 1 10.5 0Z"
                                                         clip-rule="evenodd" />
                                                 </svg>
                                             </div>
